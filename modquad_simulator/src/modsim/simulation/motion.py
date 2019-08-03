455d45673ac32f8f228d3c4374142f28c38d8df0
dsaldana <dajulian@gmail.com>
2019-08-03 13:12:54 -0400

    modquad torque control is working

--- modquad_simulator/src/modsim/simulation/motion.py
+++ modquad_simulator/src/modsim/simulation/motion.py
@@ -4,19 +4,26 @@ import numpy as np
 from modsim.util.state import stateToQd
 from math import sqrt
 
-# structure
-xx = [0, params.cage_width]
-yy = [0, 0]
-motor_roll = [[0, 0, 0, 0], [0, 0, 0, 0]]
-motor_pitch = [[0, 0, 0, 0], [0, 0, 0, 0]]
-motor_failure = [(1, 1)]  # set of tuples, (module from 0 to n-1, rotor number from 0 to 3)
 
-# xx = [0]
-# yy = [0]
-motor_failure = []  # set of tuples, (module from 0 to n-1, rotor number from 0 to 3)
+class Structure:
+
+    def __init__(self):
+        self.xx = [0, params.cage_width]
+        self.yy = [0, 0]
+        self.motor_roll = [[0, 0, 0, 0], [0, 0, 0, 0]]
+        self.motor_pitch = [[0, 0, 0, 0], [0, 0, 0, 0]]
+        self.motor_failure = [(1, 1)]  # set of tuples, (module from 0 to n-1, rotor number from 0 to 3)
+
+        # xx = [0]
+        # yy = [0]
+        self.motor_failure = []  # set of tuples, (module from 0 to n-1, rotor number from 0 to 3)
+        self.n = len(self.xx)  # Number of modules
 
 
-def crazyflie_motion(F, M):
+structure = Structure()
+
+
+def crazyflie_torquecontrol(F, M):
     """
     It receives a desired force and moment. The equation of motion of the crazyflie simulates the motor saturation.
     :param F: desired total thrust, float
@@ -54,7 +61,7 @@ def crazyflie_motion(F, M):
     return nF, nM
 
 
-def modquad_motion(F, M):
+def modquad_torquecontrol(F, M, s):
     """
     This function is similar to crazyflie_motion, but it is made for modular robots. So it specifies the dynamics
     of the modular structure. It receives a desired force and moment of a single robot.
@@ -71,7 +78,7 @@ def modquad_motion(F, M):
     rx, ry = [], []
     L = params.arm_length * sqrt(2) / 2.
 
-    for x, y in zip(xx, yy):
+    for x, y in zip(s.xx, s.yy):
         rx.append(x + L)
         rx.append(x - L)
         rx.append(x - L)
@@ -87,13 +94,12 @@ def modquad_motion(F, M):
 
     sign_rx = [1 if rx_i > 0 else -1 for rx_i in rx]
     sign_ry = [1 if ry_i > 0 else -1 for ry_i in ry]
-    # n = len(xx)  # Number of modules
     # m = 4 * n  # Number of rotors
     A = [[0.25, sy * .25 / L, -sx * .25 / L] for sx, sy in zip(sign_rx, sign_ry)]
 
     prop_thrusts = np.dot(A, [F, M[0], M[1]])  # Not using moment about Z-axis for limits
     # Failing motors
-    for mf in motor_failure:
+    for mf in s.motor_failure:
         prop_thrusts[4 * mf[0] + mf[1]]
 
     # Motor saturation
@@ -106,10 +112,11 @@ def modquad_motion(F, M):
     Mx = np.dot(ry, prop_thrusts_clamped)
     My = -np.dot(rx, prop_thrusts_clamped)
     # TODO Mz =
+
     return F, [Mx, My, M[2]]
 
 
-def state_derivative(s, F, M):
+def state_derivative(s, sF, sM):
     """
     Calculate the derivative of the state vector.
     :param t: time
@@ -118,46 +125,46 @@ def state_derivative(s, F, M):
     :param M: 3 x 1, moments output from controller (only used in simulation)
     :return: sdot: 13 x 1, derivative of state vector s
     """
-
-    F, M = modquad_motion(F, M)
-    # F, M = crazyflie_motion(F, M)
-
+    # Control of Moments and thrust
+    F, M = modquad_torquecontrol(sF, sM, structure)
+    # sF += params.mass * params.grav
+    # F, M = crazyflie_torquecontrol(sF, sM)
+    print sF, F
     # if abs(F -F1)>0.001:
     #     print 'diff'
 
-    [xdot, ydot, zdot] = s[3:6]
-    quat = s[6:10]
+    ## State of the quadrotor
+    [xdot, ydot, zdot] = s[3:6]  # Linear velocity
+    quat = s[6:10]  # orientation
     [qX, qY, qZ, qW] = quat
-    omega = s[10:]
+    bRw = quaternion_to_matrix(quat)
+    wRb = bRw.T     # Orientation in matrix form
+    omega = s[10:]  # angular velocity
     [p, q, r] = omega
 
-    bRw = quaternion_to_matrix(quat)
-    wRb = bRw.T
-
-    # Acceleration
-    gravi = np.array([0, 0, params.mass * params.grav])
-    accel = (np.dot(wRb, [0, 0, F]) - gravi) / params.mass
 
     # Angular velocity
-    K_quat = 2  # this enforces the magnitude 1 constraint for the quaternion
-    quaterror = 1 - (qW ** 2 + qX ** 2 + qY ** 2 + qZ ** 2)
+    K_quat = 2.  # this enforces the magnitude 1 constraint for the quaternion
+    quat_error = 1 - (qW ** 2 + qX ** 2 + qY ** 2 + qZ ** 2)  # Quaternion error
     qdot = (-1. / 2) * np.dot(np.array([[0, -p, -q, -r],
                                         [p, 0, -r, q],
                                         [q, r, 0, -p],
                                         [r, -q, p, 0]]), [qW, qX, qY, qZ])
-    qdot += K_quat * quaterror * np.array([qW, qX, qY, qZ])
+    qdot += K_quat * quat_error * np.array([qW, qX, qY, qZ])
+    qdot = [qdot[1], qdot[2], qdot[3], qdot[0]]  # reorder the quaternion based on the ROS quaternion representation.
 
-    #
-    qdot = [qdot[1], qdot[2], qdot[3], qdot[0]]
     # Angular acceleration
-    pqrdot = np.dot(params.invI, (M - np.cross(omega, np.dot(params.I, omega))))
+    angular_acceleration = np.dot(params.invI, (M - np.cross(omega, np.dot(params.I, omega))))
+    # Acceleration
+    gravi = np.array([0, 0, params.mass * params.grav])
+    linear_acceleration = (np.dot(wRb, [0, 0, F]) - gravi) / params.mass
 
-    # Assemble sdot
+    ## Assemble the derivative of the state
     sdot = np.zeros(13)
-    sdot[:3] = [xdot, ydot, zdot]
-    sdot[3:6] = accel
-    sdot[6:10] = qdot
-    sdot[10:] = pqrdot
+    sdot[:3] = [xdot, ydot, zdot]  # linear velocity
+    sdot[3:6] = linear_acceleration  # linear acceleration
+    sdot[6:10] = qdot  # angular velocity as a quaternion
+    sdot[10:] = angular_acceleration  # angular acceleration
 
     return sdot
 
